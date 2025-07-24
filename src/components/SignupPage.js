124c3bff50a5841a87bb3170e66588401b76c909
nidhi_500 <agrawalnidhi24@gmail.com>
2025-07-25 01:00:06 +0530

    Update SignupPage.js

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { API } from "../api";
+import API from "../api";
 import useAuthStore from "../stores/authStore";
 
 
